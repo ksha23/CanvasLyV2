20f9ec2a55df9f48a860fead05692c67571d1114
Kyle Sha <kylesha585@gmail.com>
2024-01-19 18:15:47 -0600

    remove calendar from profile page if not me

--- client/src/pages/Profile/ProfilePage.js
+++ client/src/pages/Profile/ProfilePage.js
@@ -186,13 +186,15 @@ const Profile = ({
                         <path d="M0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm14-7.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1Zm0 4a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1Zm-5-4a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1Zm0 4a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1Zm-5-4a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1Zm0 4a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1ZM20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4Z" />
                       </svg>
                     </span>
-                    <span>
-                      {profile.calendars && profile.calendarId == '' && 'No Calendar Selected'}
-                      {profile.calendars &&
-                        profile.calendarId !== '' &&
-                        profile.calendars.find((calendar) => calendar?.id === profile.calendarId)
-                          .summary}
-                    </span>
+                    {me.id === profile.id && (
+                      <span>
+                        {profile.calendars && profile.calendarId == '' && 'No Calendar Selected'}
+                        {profile.calendars &&
+                          profile.calendarId !== '' &&
+                          profile.calendars.find((calendar) => calendar?.id === profile.calendarId)
+                            .summary}
+                      </span>
+                    )}
                   </div>
                   <>
                     <div>
